23cc8b3ec34a93a1ed66894e3883b095419593f4
Lukasz Korzybski <l.korzybski@kainos.com>
2015-07-23 08:38:00 +0100

    modules exporting constructor starts with capital letter

--- src/controllers/welcomeController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var sys = require('sys');
-
-module.exports = function WelcomeController() {
-    this.welcome = function(req, res) {        
-        res.render('welcome');
-    };   
-};
