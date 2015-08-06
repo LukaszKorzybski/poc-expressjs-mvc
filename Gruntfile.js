96f000c7c0e08a59943a34df3279381a0fae7ee5
Lukasz Korzybski <l.korzybski@kainos.com>
2015-08-06 09:30:13 +0100

    added jasmine grunt plugin for running unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,21 @@ module.exports = function(grunt) {
         },
         jshint: {
             src: ['src/**/*.js', 'test/**/*.js']
+        },
+        jasmine_nodejs: {
+            app: {
+                specs: [ 'test/**' ]
+            }   
         }
     });
 
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jasmine-nodejs');
 
     grunt.registerTask('analyze', ['jshint']);
+    grunt.registerTask('test', ['jasmine_nodejs']);
     grunt.registerTask('run', [ 'express:app', 'watch:app' ]);
     grunt.registerTask('run-backend', [ 'express:backend', 'watch:backend' ]);
 };
---
+++ test/framework/errorUtils.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('debugErrorHandler', function() {
+    it('dummy test', function() {
+
+    });
+});
